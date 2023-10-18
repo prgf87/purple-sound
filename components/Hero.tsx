e86ff1ee70943e5261ba22b1e7aa76b98e18f7b4
Pedro <50713178+prgf87@users.noreply.github.com>
2023-10-18 17:21:00 +0100

    adds hero

---
+++ components/Hero.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import React from 'react';
+import img from '@/public/images/img2.jpg';
+import Link from 'next/link';
+
+type Props = {};
+
+function Hero({}: Props) {
+  return (
+    <div className="absolute left-0 top-0 right-0 bottom-0 flex justify-center items-center overflow-hidden">
+      <Image src={img} alt="/" className="w-[100%]" />
+      <div className="absolute left-0 top-0 right-0 bottom-0 bg-black/70" />
+      <div className="absolute flex">
+        <h1 className="text-7xl">Purple Sound</h1>
+
+        <h2 className="text-xs pt-1 uppercase">Company</h2>
+      </div>
+      <p></p>
+      <Link href="/contact" className="btn absolute bottom-20">
+        Contact Me
+      </Link>
+    </div>
+  );
+}
+
+export default Hero;
