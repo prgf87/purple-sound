90b1b3b8e2b09e9bc15533362d5d743bbbb6d0dc
Pedro <50713178+prgf87@users.noreply.github.com>
2023-10-18 18:59:37 +0100

    video background

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 type Props = {};
 
 function Intro({}: Props) {
-  return <div className="h-screen">Intro</div>;
+  return <div className="relative h-[100vh] top-20">Intro</div>;
 }
 
 export default Intro;
