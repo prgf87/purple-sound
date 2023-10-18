3552b0daf087c564d3736100754a7bf49af824cd
Pedro <50713178+prgf87@users.noreply.github.com>
2023-10-18 20:01:41 +0100

    end of day

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 type Props = {};
@@ -6,6 +7,23 @@ function Header({}: Props) {
   return (
     <div className="flex justify-between items-center py-6 border-b border-spacing-2 border-[#eee]/30 shadow-xl bg-[#33294A] px-10">
       <div>Logo</div>
+      <div>
+        <ul className="flex space-x-5">
+          <li className="header-link">
+            <Link href={'/'}>Home </Link>
+          </li>
+          <li className="header-link">
+            <Link href={'/about'}>About </Link>
+          </li>
+
+          <li className="header-link">
+            <Link href={'/services'}>Services </Link>
+          </li>
+          <li className="header-link">
+            <Link href={'/contact'}>Contact </Link>
+          </li>
+        </ul>
+      </div>
       <div>Menu</div>
     </div>
   );
