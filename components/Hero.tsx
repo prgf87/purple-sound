2aa0a32b217135c462ab3e2f625ead5903efc009
Pedro <50713178+prgf87@users.noreply.github.com>
2023-10-19 16:25:15 +0100

    adds layout component and catch all route

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,32 +7,30 @@ type Props = {};
 
 function Hero({}: Props) {
   return (
-    <div>
-      <div className="h-[100vh] w-[100vw] relative left-0 top-0 right-0 bottom-0 flex justify-center items-center overflow-hidden">
-        <video
-          muted
-          playsInline
-          autoPlay
-          loop
-          className="object-cover h-[100vh] scale-125"
-        >
-          <source src="/video/bgvideo.mp4" type="video/mp4" />
-        </video>
-        <div className="absolute left-0 top-0 right-0 bottom-0 bg-black/70" />
-        <div className="absolute flex">
-          <h1 className="text-7xl">Purple Sound</h1>
+    <div className="h-[100vh] w-[100vw] relative left-0 top-0 right-0 bottom-0 flex justify-center items-center overflow-hidden">
+      <video
+        muted
+        playsInline
+        autoPlay
+        loop
+        className="object-cover h-[100vh] scale-125"
+      >
+        <source src="/video/bgvideo.mp4" type="video/mp4" />
+      </video>
+      <div className="absolute left-0 top-0 right-0 bottom-0 bg-black/70" />
+      <div className="absolute flex">
+        <h1 className="text-7xl">Purple Sound</h1>
 
-          <h2 className="text-xs pt-1 uppercase">Company</h2>
-        </div>
-        <p></p>
-        <ContactButton />
-        <Link
-          href={'#intro'}
-          className="absolute bottom-2 animate-bounce text-white"
-        >
-          <SlArrowDown />
-        </Link>
+        <h2 className="text-xs pt-1 uppercase">Company</h2>
       </div>
+      <p></p>
+      <ContactButton />
+      <Link
+        href={'#intro'}
+        className="absolute bottom-20 animate-bounce text-white"
+      >
+        <SlArrowDown />
+      </Link>
     </div>
   );
 }
