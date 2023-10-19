8e22ebb3c5258aa15c05e4d3b1d5034bd2d487ad
Pedro <50713178+prgf87@users.noreply.github.com>
2023-10-19 19:44:07 +0100

    separating concerns

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import data from '@/data/intro.json';
-import Image from 'next/image';
-import Link from 'next/link';
+import Brand from './Brand';
 
 type Props = {};
 
@@ -19,17 +18,11 @@ function Brands({}: Props) {
           .map((brand, i) => {
             return (
               <div key={i} className="pt-10">
-                <Link href={brand.link} target="_blank">
-                  <Image
-                    src={brand.image}
-                    alt={brand.name}
-                    width={160}
-                    height={50}
-                    className="object-contain max-h-20 pb-4"
-                    style={{ color: 'white' }}
-                  />
-                  {/* <span className="btn2">{brand.name}</span> */}
-                </Link>
+                <Brand
+                  image={brand.image}
+                  link={brand.link}
+                  name={brand.name}
+                />
               </div>
             );
           })}
---
+++ components/Process.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+function Process({}: Props) {
+  return <div>Process</div>;
+}
+
+export default Process;
