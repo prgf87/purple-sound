743b8f5eee9cd1fa09fe6b2d8a756b947749438c
Pedro <50713178+prgf87@users.noreply.github.com>
2023-10-19 18:16:30 +0100

    new banner data

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import ContactButton from './ContactButton';
 
-type Props = { customBg: string; heading: string };
+type Props = { customBg: string; heading: string; message: string };
 
-function Banner({ customBg, heading }: Props) {
+function Banner({ customBg, heading, message }: Props) {
   return (
     <div
-      className={`${customBg} h-[40vh] z-0 flex justify-center items-center`}
+      className={`${customBg} h-[60vh] z-0 flex justify-center items-center`}
     >
-      <div className="w-full bg-[#33294A]/50 z-[2] h-full" />
-      <h1 className="absolute text-white text-7xl z-10">{heading}</h1>
+      <div className="w-full bg-[#33294A]/70 z-[2] h-full " />
+      <div className="absolute z-10 grid mx-auto">
+        <h1 className="text-white text-7xl text-center drop-shadow-2xl pb-10">
+          {heading}
+        </h1>
+        <div className="uppercase px-28">
+          <h2 className="text-white text-3xl text-center tracking-tighter">
+            {message}
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
