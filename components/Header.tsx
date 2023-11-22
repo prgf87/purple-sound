f6d46828c69610e3a12ff38b6e6b25fb11d09324
Pedro <50713178+prgf87@users.noreply.github.com>
2023-11-22 20:53:54 +0000

    recaptcha completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,7 @@ function Header({}: Props) {
           className="object-cover h-10 w-10 rounded-full"
         />
       </Link>
-      <div>
+      <div className="hidden sm:flex">
         <ul className="flex space-x-5">
           <li className="header-link">
             <Link href={'/'}>Home</Link>
@@ -68,6 +68,27 @@ function Header({}: Props) {
             <Link href={'/#contact'}>Contact </Link>
           </li>
         </ul>
+      </div>
+      <div>
+        <button className="inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 w-10 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke={textColor}
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="w-6 h-6"
+          >
+            <line x1="4" x2="20" y1="12" y2="12"></line>
+            <line x1="4" x2="20" y1="6" y2="6"></line>
+            <line x1="4" x2="20" y1="18" y2="18"></line>
+          </svg>
+          <span className="sr-only">Open menu</span>
+        </button>
       </div>
     </div>
   );
