c2aef6ca50ab9541cca676512a7d6750e095e2eb
Pedro <50713178+prgf87@users.noreply.github.com>
2023-11-22 18:27:23 +0000

    working on recaptcha

---
+++ pages/api/mail.js
@@ -0,0 +1,35 @@
+import { sendMail } from '../../utils/mailer';
+
+const handler = async (req, res) => {
+  const { name, subject, email, message, companyName, location, date } =
+    req.body.data;
+
+  try {
+    const { method } = req;
+    switch (method) {
+      case 'POST':
+        await sendMail(
+          name,
+          subject,
+          email,
+          message,
+          companyName,
+          location,
+          date
+        );
+        return res.status(200).json({ message: 'Email sent successfully' });
+      default:
+        return res.status(405).json({
+          error_code: 'method-not-allowed',
+          message: `Method ${method} Not Allowed`,
+        });
+    }
+  } catch (err) {
+    console.error(err);
+    return res
+      .status(500)
+      .json({ error_code: 'server-error', message: 'Internal server error' });
+  }
+};
+
+export default handler;
