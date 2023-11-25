c4ade179915fa7863378cf9915e04df4bddcbe0f
Pedro <50713178+prgf87@users.noreply.github.com>
2023-11-25 18:20:04 +0000

    working on captcha workflow

--- pages/api/captcha.js
+++ pages/api/captcha.js
@@ -1,25 +1,22 @@
-const captchaKey = process.env.RECAPTCHA_SECRET;
+const key = process.env.RECAPTCHA_SECRET;
 
 export default async function handler(req, res) {
-  console.log(captchaKey);
   const { token } = req.body;
   const response = await fetch(
     'https://www.google.com/recaptcha/api/siteverify',
     {
       method: 'POST',
-      secret: captchaKey,
-      response: token,
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: `secret=${key}&response=${token}`,
     }
   );
 
   const parsedResponse = await response.json();
 
+  console.log('###RESPONSE: ', Object.keys(parsedResponse));
   console.log('###RESPONSE: ', parsedResponse);
-  if (response.status === 200) {
-    // return res.status(200).send({ message: 'Success' });
+  if (parsedResponse.success === true) {
+    return res.status(200).send({ message: 'Success' });
   } else {
     return res
       .status(402)
