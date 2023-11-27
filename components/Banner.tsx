e467a97770faf04588654a2dd8dbcc46037e9d79
Pedro <50713178+prgf87@users.noreply.github.com>
2023-11-27 09:07:17 +0000

    changes text size in banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactButton from './ContactButton';
+import { CldImage } from 'next-cloudinary';
 
 type Props = { customBg: string; heading: string; message: string };
 
@@ -8,13 +9,13 @@ function Banner({ customBg, heading, message }: Props) {
     <div
       className={`${customBg} h-[60vh] z-0 flex justify-center items-center`}
     >
-      <div className="w-full bg-[#33294A] opacity-30 z-0 h-full " />
+      <div className="w-full bg-[#33294A] opacity-40 z-0 h-full " />
       <div className="absolute z-[1] grid mx-auto max-w-5xl">
-        <h1 className="text-white text-7xl text-center drop-shadow-2xl pb-10">
+        <h1 className="text-white text-5xl sm:text-7xl text-center drop-shadow-2xl pb-10">
           {heading}
         </h1>
         <div className="px-4 sm:px-28">
-          <p className="text-white font-bold text-3xl text-center tracking-tighter">
+          <p className="text-white font-bold text-xl sm:text-3xl text-center tracking-tighter">
             {message}
           </p>
         </div>
