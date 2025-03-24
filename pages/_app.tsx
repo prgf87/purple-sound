01466b9875f66818bec57f5203e1d7f4aa4a0605
Pedro <prgf2011@gmail.com>
2025-03-24 08:11:59 +0000

    fixes logo not showing up in chrome tab

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,21 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import { ReCaptchaProvider } from "next-recaptcha-v3";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTHA_SITE_KEY}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ReCaptchaProvider>
+    <>
+      <Head>
+        <title>Purple Sound Company</title>
+      </Head>
+      <ReCaptchaProvider
+        reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTHA_SITE_KEY}
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ReCaptchaProvider>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,10 +2,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" className="scroll-smooth">
+    <Html lang="en">
       <Head>
+        {/* Metadata */}
+        <meta name="application-name" content="Purple Sound Company" />
+        <meta name="description" content="Purple Sound Company" />
+        <meta name="generator" content="Next.js" />
+        <meta name="author" content="Pedro Ferreira" />
+        <meta name="publisher" content="Pedro Ferreira" />
+        <meta name="creator" content="Pedro Ferreira" />
+
+        {/* Favicon */}
+        <link rel="jpeg" type="image/jpeg" sizes="any" href="/favicon.ico" />
+
+        {/* Fonts */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
           rel="stylesheet"
